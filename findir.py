c77dea42a53bdb568ccdac1247dca03c2946dfd9
Tom Fogal <tfogal@alumni.unh.edu>
2011-02-22 18:43:25 -0700

    Provide accessors for FIN file metadata.
    Also add a bit of verification in there: there's a filename
    embedded in the FIN files which should be the same for all the
    files in a single run... as far as I can tell.

--- findir.py
+++ findir.py
@@ -41,32 +41,104 @@ class FINDir:
         ff = fin.FIN(f)
         ff.set_time_offset(last_time)
 
+        # Cache some metadata clients sometimes want to query.
+        if self._elements is None: self._elements = ff.elements()
+        if self._runfilename is None: self._runfilename = ff.run_filename()
+
+        # All the run filenames should be the same... else the user is mixing
+        # data from different data sets.
+        if self._runfilename != ff.run_filename() and \
+           os.getenv("BUB_NO_VALIDATE") is None:
+          raise UserWarning("I saw a 'run filename' (3rd line of a FIN file) "
+                            "of " + self._runfilename + ", but I'm looking at "
+                            + f + " right now, and it has a run filename of "
+                            + ff.run_filename() + ".  This probably means you "
+                            "are mixing FIN files from logically separate "
+                            "data sets.\n"
+                            "You can set the environment variable "
+                            "'BUB_NO_VALIDATE' to get around this, but you're "
+                            "probably processing unassociated data together, "
+                            "which does not make sense.")
+
         elem = ff.element(element_name)
         if element_name is "Time": last_time = max(elem)
         data.extend(elem)
 
       return data
 
+  def date(self):
+    '''Returns the date in the first FIN file.'''
+    if self._date is None:
+      # User hasn't called 'element' yet; we need to parse it =(
+      ff = fin.FIN(self._files()[0])
+      self._date = ff.date()
+    assert(self._date is not None)
+    return self._date
+
+  def elements(self):
+    '''Returns the list of elements in this data set.'''
+    if self._elements is None:
+      ff = fin.FIN(self._files()[0])
+      self._elements = ff.elements()
+    return self._elements
+
+  def run_filename(self):
+    '''The "run filename" is the 3rd line of the FIN files.  For a single run
+    (i.e. all the associated files in a FINDir), all the run filenames should
+    be the same.'''
+    if self._runfilename is None:
+      ff = fin.FIN(self._files()[0])
+      self._runfilename = ff.run_filename()
+    return self._runfilename
+
 if __name__ == "__main__":
   import unittest
 
   class TestFINDir(unittest.TestCase):
+    '''There's a significant issue with testing this: the user needs a
+    directory full of FIN files.  I've hardcoded a directory I have in here for
+    now, unfortunately.  Sorry...'''
+
+    def setUp(self):
+      self._fd = FINDir(os.sep.join([os.getenv("HOME"), "data",
+                                     "bu-biometallomics", "BottomLeft",
+                                     "TOPLEFTFIN2"]), "ABC*FIN2")
+
     def test_element_entries(self):
-      fd = FINDir(os.sep.join([os.getenv("HOME"), "data", "bu-biometallomics",
-                              "BottomLeft", "TOPLEFTFIN2"]), "ABC*FIN2")
-      self.assertEqual(len(fd.element("Li7")), 22099)
+      self.assertEqual(len(self._fd.element("Li7")), 22099)
 
     def test_element_time_increasing(self):
       '''Time should monotonically increase.'''
-      fd = FINDir(os.sep.join([os.getenv("HOME"), "data", "bu-biometallomics",
-                              "BottomLeft", "TOPLEFTFIN2"]), "ABC*FIN2")
-      t = fd.element("Time")
+      t = self._fd.element("Time")
       for i in xrange(1, len(t)): self.assertTrue(t[i-1] < t[i])
 
     def test_invalid_element(self):
       '''Invalid element names should throw an exception.'''
-      fd = FINDir(os.sep.join([os.getenv("HOME"), "data", "bu-biometallomics",
-                              "BottomLeft", "TOPLEFTFIN2"]), "ABC*FIN2")
-      self.assertRaises(IndexError, fd.element, "ThisIsGarbage!")
+      self.assertRaises(IndexError, self._fd.element, "ThisIsGarbage!")
+
+    def test_date(self):
+      self.assertEqual(self._fd.date(), "Friday, October 15, 2010 21:49:14")
+
+    def test_elements(self):
+      '''Test explicit elements query.'''
+      el = ["Time", "Li7", "Ca44", "Mn55", "Cu63", "Zn66", "Zn70", "As75",
+            "Se77", "Se82", "Rb85", "Mo95", "Ce140", "Au197", "Pb207", "Pb208",
+            "P31"]
+      self.assertEqual(len(self._fd.elements()), len(el))
+      for i in xrange(0, len(el)):
+        self.assertEqual(self._fd.elements()[i], el[i])
+
+    def test_elements_data(self):
+      '''Test reading the elements while reading data.'''
+      el = ["Time", "Li7", "Ca44", "Mn55", "Cu63", "Zn66", "Zn70", "As75",
+            "Se77", "Se82", "Rb85", "Mo95", "Ce140", "Au197", "Pb207", "Pb208",
+            "P31"]
+      self.assertEqual(len(self._fd.element("Rb85")), 22099)
+      self.assertEqual(len(self._fd.elements()), len(el))
+      for i in xrange(0, len(el)):
+        self.assertEqual(self._fd.elements()[i], el[i])
+
+    def test_runfilename(self):
+      self.assertEqual(self._fd.run_filename(), "101510lm2.FIN")
 
   unittest.main()
